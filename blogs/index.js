d42faf4bdf95f42608accb9378773b8d2ee77acb
sulavajuusto <esko.salminen@gmail.com>
2024-02-25 09:43:04 +0200

    exercise 4.2

--- blogs/index.js
+++ blogs/index.js
@@ -1,13 +1,6 @@
+const app = require('./app');
 const config = require('./utils/config');
-const express = require('express');
-const app = express();
-const cors = require('cors');
 const logger = require('./utils/logger');
-const blogsRouter = require('./controllers/blogs.js');
-
-app.use(cors());
-app.use(express.json());
-app.use('/api/blogs', blogsRouter);
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
